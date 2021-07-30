8f1b40d3bf713ccbaf4f82befba657ccd89de84c
Paula Anaya <paulaanayaramirez27@gmail.com>
2021-07-30 11:51:00 -0500

    Creacion del primer banner

--- website/src/components/banner/Banner.js
+++ website/src/components/banner/Banner.js
@@ -7,28 +7,46 @@ import "./Banner.css";
 function Banner() {
   return (
     <div className="bg">
-      <div className="row ">
-        <div className="col-lg-6 col-md-12 col-sm-12 align" >
+      <div className="row">
+        <div className="col-lg-6 col-md-12 col-sm-12 align ">
           <h1 className="fontSemiBold">
-            Animate Your ideas, Design Better Apss
+            Animate Your ideas, Design Better Apps
           </h1>
           <br></br>
           <p className="fontMedium">
-            Make it easy to desing animated and interactive user interfaces, 
+            Make it easy to desing animated and interactive user interfaces,
             Work done for an upcoming NFT marketplace catered to musicians.
           </p>
           <br></br>
-          <div className="row">
-            <div className="col-12 fontSemiBold"> 
+          <div className="row ">
+            <div className="col-12 fontSemiBold">
               <Button message="Buy Token" style="intraLightBtn margin" />
               <Button message="See Whitepaper" style="intraDarkBtn " />
             </div>
           </div>
+          <br></br>
+          <br></br>
+
+          <div className="row text-center">
+            <div className="col block">
+              <h4>100M</h4>
+              <p>Pre Sale Token</p>
+            </div>
+            <div className="col block">
+              <h4>250</h4>
+              <p>Total Supply</p>
+            </div>
+            <div className="col ">
+              <h4 className="text-green">250</h4>
+              <p>Market Cap</p>
+            </div>
+          </div>
         </div>
-        <div className="col-lg-6 col-md-6 d-sm-none d-lg-block" align="center">
-          <img className="d-sm-none  d-xs-none d-lg-block  "   width="80%" src={Hand}/>
+        <div className="col-lg-6 d-lg-block col-md-6 hide" align="center">
+          <img width="80%" src={Hand} />
         </div>
       </div>
+
     </div>
   );
 }
