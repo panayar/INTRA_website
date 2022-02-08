18fc5269717aae635cb6b619985e1240ae1a63e3
Paula Anaya <paula.anaya@itac.co>
2022-02-08 12:55:10 -0500

    fixing url

--- website/src/App.js
+++ website/src/App.js
@@ -2,11 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/global.css";
 import Footer from "./components/footers/Footer";
 import Nav from "./components/Navs/Nav";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div className="container">
      <Nav modalId ="mainForm" modalBtn="#mainForm"/>
+     <Main/>
      <Footer/>
     </div>
   );
